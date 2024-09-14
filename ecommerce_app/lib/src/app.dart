8143dd1981274d69ec895aab13e19c35e35deb45
Jacob Beauchamp <jacob.beauchamp75@gmail.com>
2024-09-14 16:32:20 -0500

    changed naming to Boost

--- ecommerce_app/lib/src/app.dart
+++ ecommerce_app/lib/src/app.dart
@@ -13,7 +13,7 @@ class MyApp extends ConsumerWidget {
       routerConfig: goRouter,
       debugShowCheckedModeBanner: false,
       restorationScopeId: 'app',
-      onGenerateTitle: (BuildContext context) => 'Jake\'s Data'.hardcoded,
+      onGenerateTitle: (BuildContext context) => 'Boost'.hardcoded,
       theme: ThemeData(
         // * Use this to toggle Material 3 (defaults to true since Flutter 3.16)
         useMaterial3: true,
